4010a7d074326075a488eb82c067b08878659f0b
wujingxuan227 <1399877577@qq.com>
2020-12-02 16:55:41 +0800

    commit 2

--- wjx.java
+++ wjx.java
@@ -1,5 +1,6 @@
-import java.util.*;
-class teacher{
-   String name;  //老师姓名
-   String tno;   //老师工号
+import java.util.*
+class student{
+  private string name;
+  private string address;
+  private string email;   //修改的内容
 }
